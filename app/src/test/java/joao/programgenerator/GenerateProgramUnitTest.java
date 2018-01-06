9e2b823d74e6c00fdbd675fca0fae5b97978c793
Joao Ferreira <maneca59@hotmail.com>
2018-01-06 16:06:09 +0000

    Started working on unit testing using Roboelectric

---
+++ app/src/test/java/joao/programgenerator/GenerateProgramUnitTest.java
@@ -0,0 +1,29 @@
+package joao.programgenerator;
+
+import android.content.Intent;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.shadows.ShadowApplication;
+
+import joao.programgenerator.activities.MainActivity;
+import joao.programgenerator.activities.ProgramaActivity;
+
+import static junit.framework.Assert.assertEquals;
+
+@RunWith(RobolectricTestRunner.class)
+public class GenerateProgramUnitTest {
+
+    @Test
+    public void generateProgram(){
+
+        MainActivity mainActivity = Robolectric.setupActivity(MainActivity.class);
+        mainActivity.findViewById(R.id.gerar).performClick();
+
+        Intent expectedIntent = new Intent(mainActivity, ProgramaActivity.class);
+        Intent actual = ShadowApplication.getInstance().getNextStartedActivity();
+        assertEquals(expectedIntent.getComponent(), actual.getComponent());
+    }
+}
